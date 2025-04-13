e59cec7832c445fe78097ca2f97e9f1dd6c0b133
LakAgarwal <lak.agarwal04@gmail.com>
2025-04-13 12:03:25 +0530

    Updating

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,20 +1,27 @@
-import React from 'react';
 import { useLocation } from "react-router-dom";
 import { useEffect } from "react";
 
-function NotFound() {
-  let l = useLocation()
+const NotFound = () => {
+  const location = useLocation();
+
   useEffect(() => {
-    console.log("oops lol", l.pathname)
-  })
+    console.error(
+      "404 Error: User attempted to access non-existent route:",
+      location.pathname
+    );
+  }, [location.pathname]);
 
   return (
-    <div style={{ minHeight: "100px", display: "block", background: "pink" }}>
-      <h5>404</h5>
-      <h2>uhhh this page is gone i guess</h2>
-      <a href="/" style={{ color: "green", fontWeight: "lighter", fontSize: 10 }}>Go Home Maybe?</a>
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <div className="text-center">
+        <h1 className="text-4xl font-bold mb-4">404</h1>
+        <p className="text-xl text-gray-600 mb-4">Oops! Page not found</p>
+        <a href="/" className="text-blue-500 hover:text-blue-700 underline">
+          Return to Home
+        </a>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default NotFound
+export default NotFound;
